2aaf12fc57e02aa277ef7c6967c474cc020d555d
farthen <wilkefi@googlemail.com>
2013-03-15 00:05:38 +0100

    Merge FACache and FACachedItem, improve README

--- FACache.h
+++ FACache.h
@@ -1,13 +1,11 @@
 //
 //  FACache.h
-//  Trakr
 //
 //  Created by Finn Wilke on 13.03.13.
 //  Copyright (c) 2013 Finn Wilke. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import "FACacheableItem.h"
 
 @interface FACache : NSCache <NSCacheDelegate, NSCoding>
 
@@ -47,5 +45,23 @@
 
 // Triggered when the timer has fired for the object with key
 - (void)timerElapsedForKey:(id)key;
+
+@end
+
+@interface FACachedItem : NSObject <NSCoding>
+
+- (id)initWithCache:(FACache *)cache key:(id)key object:(id)object;
+
+@property (readonly) id cacheKey;
+@property (assign) NSTimeInterval expirationTime;
+
+@property (assign) NSUInteger cost;
+
+@property (readonly) id object;
+
+- (void)setTimer;
+- (void)removeTimer;
+- (void)removeExpirationData;
+- (BOOL)objectHasExpired;
 
 @end
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 ## Objective-C Util
 This is a small collection of Objective-C code I like to use in projects.
 
+## Usage
+### FACache
+This is an NSCache subclass to support a timer after which the cached item is removed. It also supports NSCoding so that it can be saved to disk easily. Just read through the header file.
+
 ## License
 
 	------------------------------------------------------------------------
